34920186b4182e78313c2dfd10a29b8b87842b31
JahShoeAh <joshuacho52005@gmail.com>
2025-09-15 19:56:11 -0400

    Initial Commit

---
+++ sports/sportsApp.swift
@@ -0,0 +1,17 @@
+//
+//  sportsApp.swift
+//  sports
+//
+//  Created by Josh Cho on 9/15/25.
+//
+
+import SwiftUI
+
+@main
+struct sportsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ sportsTests/sportsTests.swift
@@ -0,0 +1,17 @@
+//
+//  sportsTests.swift
+//  sportsTests
+//
+//  Created by Josh Cho on 9/15/25.
+//
+
+import Testing
+@testable import sports
+
+struct sportsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
